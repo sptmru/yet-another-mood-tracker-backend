94e78181e22845efba243bebc953075ca7b0f39f
Soslan Aldatov <soslanaldatov@gmail.com>
2025-02-09 03:00:10 +0400

    add schemas for mood without reasons included; add update mood schema

--- src/domain/definitions/mood.definition.ts
+++ src/domain/definitions/mood.definition.ts
@@ -1,7 +1,6 @@
 export const MoodDefinition = {
   $id: 'Mood',
   type: 'object',
-  name: 'Mood',
   properties: {
     id: { type: 'string' },
     date: { type: 'string' },
@@ -16,4 +15,34 @@ export const MoodDefinition = {
     createdAt: { type: 'string' },
   },
   required: ['date', 'rating'],
+};
+
+export const MoodWithoutReasonsDefinition = {
+  $id: 'MoodWithoutReasons',
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+    date: { type: 'string' },
+    rating: { type: 'number' },
+    description: { type: 'string' },
+    createdAt: { type: 'string' },
+  },
+  required: ['date', 'rating'],
+};
+
+export const UpdateMoodDTODefinition = {
+  $id: 'UpdateMoodDTO',
+  type: 'object',
+  properties: {
+    date: { type: 'string' },
+    rating: { type: 'number' },
+    description: { type: 'string' },
+    reasonIds: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+    },
+  },
+  required: [],
 };
