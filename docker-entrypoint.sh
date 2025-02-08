dd1a115d66ffc1ee1c4d99a91e0f594c2978f329
Soslan Aldatov <soslanaldatov@gmail.com>
2025-02-08 23:05:08 +0400

    add Loki and Graylog parameters to app configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,20 @@
 echo "Updating .env"
 sed -i "s|LOG_LEVEL=debug|LOG_LEVEL=${LOG_LEVEL:-info}|" ./build/.env
 
+sed -i "s|LOKI_ENABLED=false|LOKI_ENABLED=${LOKI_ENABLED:-false}|" ./build/.env
+sed -i "s|LOKI_ENDPOINT=http://loki-gateway.svc.cluster.local|LOKI_ENDPOINT=${LOKI_ENDPOINT:-http://loki-gateway.svc.cluster.local}|" ./build/.env
+sed -i "s|LOKI_LABEL_JOB=yet-another-mood-tracker-api|LOKI_LABEL_JOB=${LOKI_LABEL_JOB:-yet-another-mood-tracker-api}|" ./build/.env
+sed -i "s|LOKI_JSON=false|LOKI_JSON=${LOKI_JSON:-false}|" ./build/.env
+sed -i "s|LOKI_INTERVAL=5|LOKI_INTERVAL=${LOKI_INTERVAL:-5}|" ./build/.env
+sed -i "s|LOKI_TIMEOUT=10000|LOKI_TIMEOUT=${LOKI_TIMEOUT:-10000}|" ./build/.env
+
+sed -i "s|GRAYLOG_ENABLED=false|GRAYLOG_ENABLED=${GRAYLOG_ENABLED:-false}|" ./build/.env
+sed -i "s|GRAYLOG_HOST=graylog|GRAYLOG_HOST=${GRAYLOG_HOST:-graylog}|" ./build/.env
+sed -i "s|GRAYLOG_PORT=12201|GRAYLOG_PORT=${GRAYLOG_PORT:-12201}|" ./build/.env
+sed -i "s|GRAYLOG_HOSTNAME=graylog|GRAYLOG_HOSTNAME=${GRAYLOG_HOSTNAME:-graylog}|" ./build/.env
+sed -i "s|GRAYLOG_FACILITY=yet-another-mood-tracker-api|GRAYLOG_FACILITY=${GRAYLOG_FACILITY:-yet-another-mood-tracker-api}|" ./build/.env
+sed -i "s|GRAYLOG_BUFFER_SIZE=1400|GRAYLOG_BUFFER_SIZE=${GRAYLOG_BUFFER_SIZE:-1400}|" ./build/.env
+
 # Start service
 echo "Starting app"
 cd build && node app.js
--- src/infrastructure/config/config.ts
+++ src/infrastructure/config/config.ts
@@ -9,4 +9,23 @@ export const config = {
     file: `${parsedConfig?.LOG_LEVEL != null ? parsedConfig.LOG_LEVEL : 'debug'}.log`,
     logToFile: parsedConfig?.LOG_TO_FILE != null ? parsedConfig.LOG_TO_FILE.toLowerCase() === 'true' : false,
   },
+  loki: {
+    enabled: parsedConfig?.LOKI_ENABLED != null ? parsedConfig.LOKI_ENABLED.toLowerCase() === 'true' : false,
+    host:
+      parsedConfig?.LOKI_ENDPOINT != null
+        ? parsedConfig.LOKI_ENDPOINT
+        : 'http://loki-gateway.svc.cluster.local',
+    labels: { job: parsedConfig?.LOKI_LABEL_JOB != null ? parsedConfig.LOKI_LABEL_JOB : 'yet-another-mood-tracker-api' },
+    json: parsedConfig?.LOKI_JSON != null ? parsedConfig.LOKI_JSON.toLowerCase() === 'true' : false,
+    interval: parsedConfig?.LOKI_INTERVAL != null ? Number(parsedConfig.LOKI_INTERVAL) : 5,
+    timeout: parsedConfig?.LOKI_TIMEOUT != null ? Number(parsedConfig.LOKI_TIMEOUT) : 10000,
+  },
+  graylog: {
+    enabled: parsedConfig?.GRAYLOG_ENABLED != null ? parsedConfig.GRAYLOG_ENABLED.toLowerCase() === 'true' : false,
+    host: parsedConfig?.GRAYLOG_HOST != null ? parsedConfig.GRAYLOG_HOST : 'graylog',
+    port: parsedConfig?.GRAYLOG_PORT != null ? Number(parsedConfig.GRAYLOG_PORT) : 12201,
+    hostname: parsedConfig?.GRAYLOG_HOSTNAME != null ? parsedConfig.GRAYLOG_HOSTNAME : 'graylog',
+    facility: parsedConfig?.GRAYLOG_FACILITY != null ? parsedConfig.GRAYLOG_FACILITY : 'yet-another-mood-tracker-api',
+    bufferSize: parsedConfig?.GRAYLOG_BUFFER_SIZE != null ? Number(parsedConfig.GRAYLOG_BUFFER_SIZE) : 1400,
+  },
 };
