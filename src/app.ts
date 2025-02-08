9627e00551d6eccdb65a4676fc85488ba53b7611
Soslan Aldatov <soslanaldatov@gmail.com>
2025-02-09 02:38:03 +0400

    initialize TypeORM data source; enable Mood and Reason schemas; enable Reason routes

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,32 @@
+import { MoodDefinition } from './domain/definitions/mood.definition';
+import { ReasonDefinition } from './domain/definitions/reason.definition';
 import { Api } from './infrastructure/api/server';
+import { dataSource } from './infrastructure/database/data-source';
 import { logger } from './misc/Logger';
 import { HealthRoute } from './routes/health/health.route';
+import { ReasonRoute } from './routes/reason/reason.route';
 
-const api = new Api({
-  plugins: [],
-  routes: [HealthRoute],
-  definitions: [],
-});
-api.listen();
+void (async (): Promise<void> => {
+  try {
+    await dataSource.initialize();
+    logger.debug('Data Source initialized');
+  } catch (err) {
+    logger.error('Error during Data Source initialization', err);
+  }
 
-const onShutdown = async (): Promise<void> => {
-  logger.info('Shutting down...');
-  await api.app.close();
-  process.exit(0);
-};
+  const api = new Api({
+    plugins: [],
+    routes: [HealthRoute, ReasonRoute],
+    definitions: [MoodDefinition, ReasonDefinition],
+  });
+  api.listen();
 
-process.on('SIGINT', onShutdown);
-process.on('SIGTERM', onShutdown);
+  const onShutdown = async (): Promise<void> => {
+    logger.info('Shutting down...');
+    await api.app.close();
+    process.exit(0);
+  };
+
+  process.on('SIGINT', onShutdown);
+  process.on('SIGTERM', onShutdown);
+})();
