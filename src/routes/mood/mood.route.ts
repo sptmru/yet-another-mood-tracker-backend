554dd2b26c8e41f9c04f3ae1a750bb794db508b0
Soslan Aldatov <soslanaldatov@gmail.com>
2025-02-10 16:50:06 +0400

    update mood routes to use new routes options

--- src/routes/mood/mood.route.ts
+++ src/routes/mood/mood.route.ts
@@ -1,13 +1,12 @@
 import { FastifyInstance } from 'fastify';
+
 import { MoodController } from '../../controllers/mood/mood.controller';
-import {
-  getMoodByIdRouteOptions,
-  getMoodsWithReasonsRouteOptions,
-  getMoodsRouteOptions,
-  addMoodRouteOptions,
-  updateMoodRouteOptions,
-  deleteMoodRouteOptions,
-} from './mood.route-options';
+import { getMoodsRouteOptions } from './options/get-moods.route-options';
+import { getMoodsWithReasonsRouteOptions } from './options/get-moods-with-reasons.route-options';
+import { getMoodByIdRouteOptions } from './options/get-mood-by-id.route-options';
+import { addMoodRouteOptions } from './options/add-mood.route-options';
+import { updateMoodRouteOptions } from './options/update-mood.route-options';
+import { deleteMoodRouteOptions } from './options/delete-mood.route-options';
 
 export class MoodRoute {
   public prefix = '/moods';
